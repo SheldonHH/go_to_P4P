76e5c568ec39af734dfa4879f533a45351edb962
fedproject <xphuang@smu.edu.sg>
2022-05-16 18:29:47 +0800

    add P4P go

---
+++ go_call_java_example.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "os/exec"
+    "fmt"
+)
+var data_path = "/root/user_data1.csv"
+var jar_path = "/root/P4P/P4P.jar"
+
+func main() {
+    cmd := exec.Command("java", "-jar",jar_path, "sg")
+    stdout, err := cmd.Output()	
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+
+    // Print the output
+    fmt.Println(string(stdout))
+}
